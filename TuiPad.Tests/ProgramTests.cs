3ff992b5cce082ba9f0aabf075d81f027bb39466
staieb <sxtaieb@gmail.com>
2018-09-24 17:37:18 +0200

    Creation

---
+++ TuiPad.Tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TuiPad.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TuiPad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TuiPad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+
+                Console.WriteLine($"Press any key to continue...");
+                Console.ReadKey();
+            }
+           
+        }
+    }
+}
